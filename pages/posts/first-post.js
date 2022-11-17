27d8e3c8abf4776294d3675d13d56234f1c6796e
MyeongHoonNam <codenmh0822@naver.com>
2022-11-17 14:54:20 +0900

    Link 태그를 통한 페이지 라우팅

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,15 @@
-// pages 폴더 아래의 폴더 경로가 url로 이루어진다.
+import Link from "next/link";
 
+// pages 폴더 아래의 폴더 경로가 url로 이루어진다.
 const FirstPost = () => {
-  return <h1>First Post</h1>;
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
 };
 
 export default FirstPost;
