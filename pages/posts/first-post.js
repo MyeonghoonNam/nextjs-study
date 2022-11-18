b1d8189ceb57a6d7782a6cba9825c206689634a9
MyeongHoonNam <codenmh0822@naver.com>
2022-11-18 17:15:19 +0900

    빌트인 Image 컴포넌트를 활용한 정적이미지 로드

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 // pages 폴더 아래의 폴더 경로가 url로 이루어진다.
 const FirstPost = () => {
@@ -8,6 +9,7 @@ const FirstPost = () => {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+      <Image src="/images/example.jpg" width={200} height={200} alt="profile" />
     </>
   );
 };
