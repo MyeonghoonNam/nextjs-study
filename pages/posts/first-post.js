47910deb859fb3c997af46fe707392ccadfec434
MyeongHoonNam <codenmh0822@naver.com>
2022-11-19 22:18:53 +0900

    Script 컴포넌트를 활용한 script 태그 관리

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -9,6 +10,14 @@ const FirstPost = () => {
       <Head>
         <title>First Post</title>
       </Head>
+
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
 
       <h1>First Post</h1>
       <h2>
