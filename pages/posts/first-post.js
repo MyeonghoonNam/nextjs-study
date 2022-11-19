518a78fd0080cd79468fe71249586fff1df1aa7c
MyeongHoonNam <codenmh0822@naver.com>
2022-11-19 22:25:43 +0900

    css 모듈로 적용

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,11 +2,12 @@ import Head from "next/head";
 import Script from "next/script";
 import Link from "next/link";
 import Image from "next/image";
+import Layout from "../../components/Layout";
 
 // pages 폴더 아래의 폴더 경로가 url로 이루어진다.
 const FirstPost = () => {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -24,7 +25,7 @@ const FirstPost = () => {
         <Link href="/">Back to home</Link>
       </h2>
       <Image src="/images/example.jpg" width={200} height={200} alt="profile" />
-    </>
+    </Layout>
   );
 };
 
