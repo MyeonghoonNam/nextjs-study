3173e200f57ce48326c045dc2ca594c0e7e80db6
MyeongHoonNam <codenmh0822@naver.com>
2022-11-20 20:52:36 +0900

    공통 Layout 적용

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Script from "next/script";
-import Link from "next/link";
 import Image from "next/image";
 import Layout from "../../components/Layout";
 
@@ -21,9 +20,7 @@ const FirstPost = () => {
       />
 
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+
       <Image src="/images/example.jpg" width={200} height={200} alt="profile" />
     </Layout>
   );
