94fd4f8d84127bcb5846aec6e248d604439bbbd1
MyeongHoonNam <codenmh0822@naver.com>
2022-11-23 21:20:26 +0900

    프리렌더링 적용

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,45 @@ import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-const Home = () => {
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+const Home = ({ allPostsData }) => {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
       <section className={utilStyles.headingMd}>
         <p>Hi~ There~!</p>
         <p>
           (This is a sample website{" "}
           <Link href="/posts/first-post">my first post</Link>.)
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
